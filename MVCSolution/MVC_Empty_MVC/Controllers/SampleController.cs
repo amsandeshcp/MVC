276a0e2924044d60b4537c5065d208ba67248f4c
Sandesh <amsandeshcp@gmail.com>
2021-12-31 21:05:01 +0530

    First Commit

---
+++ MVCSolution/MVC_Empty_MVC/Controllers/SampleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Empty_MVC.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SampleMenu1()
+        {
+            return Content("sam");
+        }
+
+        public ActionResult SampleMenu2()
+        {
+            return View();
+        }
+
+        public ActionResult MenuSample3()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCSolution/MVC_Empty_MVC/Controllers/clothing_and_accessoriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Empty_MVC.Controllers
+{
+    public class clothing_and_accessoriesController : Controller
+    {
+        // GET: clothing_and_accessories
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
